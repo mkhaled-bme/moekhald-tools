96da4d0e771710aa2b884f03c9385f15192d838f
Mohamed Khaled <mkhaled@bme-global.com>
2024-03-18 06:48:03 +0200

    minor

--- app/password-generator/page.js
+++ app/password-generator/page.js
@@ -6,7 +6,9 @@ import * as React from 'react';
 export default function PasswordGeneratorPage() {
     return (
         <main className='flex min-h-screen flex-col items-center px-8 py-24 sm:p-24 space-y-8'>
-            <h1 className='font-bold'>Password Generator</h1>
+            <div>
+                <h1 className='font-bold'>Password Generator</h1>
+            </div>
             <PasswordGenerator />
             <Link href='/'>
                 <Button variant='link'>Home</Button>
